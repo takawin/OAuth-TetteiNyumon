ab5807aba53a238bbce22f6aa6b1d28046104f0a
taka <taka42.win@gmail.com>
2026-07-06 19:19:47 +0900

    feat: とりあえずLambdaの前段にAPI Gatewayを置く

--- protected-resource/iac/main.tf
+++ protected-resource/iac/main.tf
@@ -27,3 +27,36 @@ resource "aws_lambda_function" "this" {
     ignore_changes = [filename, source_code_hash]
   }
 }
+
+resource "aws_apigatewayv2_api" "this" {
+  name          = var.function_name
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = aws_apigatewayv2_api.this.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.this.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
+
